1129ad40b72695680e5ca286d85c9fb5dec8d9d9
Toan Nguyen <nguyenbachtoan120997@gmail.com>
2021-06-29 17:45:59 +0700

    update package.json

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.APP_PORT || 8080);
+  await app.listen(process.env.PORT || 8080);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
